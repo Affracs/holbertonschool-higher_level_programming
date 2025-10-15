cc80c107c3b2e293d72fb3d1c343ccdbe79433de
Affracs <alexfrancois1501@gmail.com>
2025-10-15 13:54:34 +0400

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class Square with a private size attribute."""
+"""
+Defines a class Square with a private size attribute.
+"""
 
 
 class Square:
-    """Represents a square with a size."""
+    """
+    Represents a square with a size.
+    """
 
-    def _init_(self, size):
-        """Initialize the square with a given size."""
+    def __init__(self, size):
+        """
+        Initialize the square with a given size.
+        """
+        
         self.__size = size
