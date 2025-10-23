3781fa50a4dcea63f87826f8449f0f56840ebdb7
Affracs <alexfrancois1501@gmail.com>
2025-10-23 09:10:11 +0400

    0-1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-"""
+"""Defines MyList class that inherits from list and adds method to print list sorted."""
 class MyList(list):
-    """
-    """
+    """Custom list class that can print itself sorted."""
     def print_sorted(self):
-        """
-        """
+        """Print the list elements in ascending sorted order without modifying the list."""
         print(sorted(self))
