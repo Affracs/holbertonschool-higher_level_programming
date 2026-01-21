81c615e322c304bf12a59caeb19909db31148a21
Affracs <alexfrancois1501@gmail.com>
2026-01-21 11:57:55 +0400

    10

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last = number % 10
-    if number < 0 and last != 0:
-        last -= 10
-    print(last, end="")
+    if last < 0:
+        last = -last
+    print("{}".format(last), end="")
     return last
