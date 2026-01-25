e3382f1c05a399ad49f9fe5b66795d21c59de83d
Affracs <alexfrancois1501@gmail.com>
2026-01-25 21:58:46 +0400

    d

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that adds two integers."""
+
+
+def add_integer(a, b=98):
+    """Add two integers or floats casted to integers."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
