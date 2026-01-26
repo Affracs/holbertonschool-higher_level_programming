181bf3050e8ed42eea6bb25f664bf57e098182f9
Affracs <alexfrancois1501@gmail.com>
2026-01-26 13:41:06 +0400

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-"""Module that adds two integers."""
+"""
+Module that adds two integers.
+"""
 
 
 def add_integer(a, b=98):
-    """Add two integers or floats casted to integers."""
+    """
+    Adds two integers or floats (casted to int).
+
+    Raises:
+        TypeError: if a or b is not an integer or float
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    try:
+        a = int(a)
+        b = int(b)
+    except Exception:
+        # catches float overflow and weird float values
+        raise TypeError("a must be an integer")
+
+    return a + b
