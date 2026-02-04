994dcfe50cd7aa15196c55329d60f74e101bacf3
Affracs <alexfrancois1501@gmail.com>
2026-02-04 11:39:04 +0400

    haha

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Function that returns list of attributes and methods of object"""
+
+def lookup(obj):
+    """Return list of available attributes and methods of object"""
+    return dir(object)
