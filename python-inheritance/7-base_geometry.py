d09781eec7e91a4ef0942dd831ac9879c841cba1
Affracs <alexfrancois1501@gmail.com>
2026-02-15 19:19:40 +0400

    late af3

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+"""Module that defines BaseGeometry class with validation"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raise an exception since area is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
