ebb7bbef79a5be5d8d871c1b743efa3e82da6bc3
Affracs <alexfrancois1501@gmail.com>
2026-03-05 14:20:03 +0400

    9

--- python-inheritance/9-recangle.py
+++ python-inheritance/9-recangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Rectangle module"""
+"""Module that defines a Rectangle class"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -7,9 +8,18 @@ class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        """Initialize rectangle with validated width and height"""
+        """Initialize the rectangle"""
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the rectangle description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
