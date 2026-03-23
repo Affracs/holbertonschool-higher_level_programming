3657b18dd8cc1e99844ab6486a8b52fa78743101
Affracs <alexfrancois1501@gmail.com>
2026-03-23 10:51:44 +0400

    02

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module defines a VerboseList class"""
+
+
+class VerboseList(list):
+    """List that prints notifications on modifications"""
+
+    def append(self, item):
+        """Append item and print notification"""
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        """Extend list and print notification"""
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        """Remove item and print notification"""
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """Pop item and print notification"""
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
+    
