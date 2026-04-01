3100140ee825e8713d9d882301cb5ab2e72fecf8
Affracs <alexfrancois1501@gmail.com>
2026-04-01 09:50:23 +0400

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""Module to append a string at the end of a text"""
+
+
+
 def append_write(filename="", text=""):
+    """Appends a string at the end of a UTF-8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The text to append to the file.
+
+    Returns:
+        int: The number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
