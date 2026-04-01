420fa4a549f3496b21c7f096504e586b57a04160
Affracs <alexfrancois1501@gmail.com>
2026-04-01 14:45:51 +0400

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+"""Script that adds all arguments to a Python list and saves them to a JSON file."""
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    data = load_from_json_file(filename)
-except:
-    data = []
+    my_list = load_from_json_file(filename)
+except Exception:
+    my_list = []
 
-data.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])
 
-save_to_json_file(data, filename)
+save_to_json_file(my_list, filename)
