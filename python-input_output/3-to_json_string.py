17551c1a321a61ffc9735d5895c03b83181875d7
Affracs <alexfrancois1501@gmail.com>
2026-04-01 10:36:02 +0400

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Module for converting a Python object to a JSON string."""
+
+
 import json
 
+
 def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The Python object to convert.
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
     return json.dumps(my_obj)
