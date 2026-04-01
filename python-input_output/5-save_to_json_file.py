373761d78e539b8ecabcfe2ea13aa9dd071df7c0
Affracs <alexfrancois1501@gmail.com>
2026-04-01 14:24:11 +0400

    5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Module for converting a JSON string to a Python object."""
+"""Module for saving a Python object to a JSON file."""
 
 import json
 
 
-def from_json_string(my_str):
-    """Returns the Python object represented by a JSON string.
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using a JSON representation.
 
     Args:
-        my_str (str): The JSON string to convert.
+        my_obj: The Python object to serialize.
+        filename (str): The name of the file to write to.
 
     Returns:
-        object: The Python object resulting from the JSON string.
+        None
     """
-    # Dummy with to satisfy checker requirement
-    with open(__file__, "r", encoding="utf-8"):
-        pass
-
-    return json.loads(my_str)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
