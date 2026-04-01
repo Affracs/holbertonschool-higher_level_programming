9e5babaec3647e5a1ca94d5af8c0758077722347
Affracs <alexfrancois1501@gmail.com>
2026-04-01 09:41:26 +0400

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns number of characters."""
+    """Writes a string to a UTF-8 text file and returns number of characters"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
