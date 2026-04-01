303adba200ad215fac035982b68469cf7e6bf7b0
Affracs <alexfrancois1501@gmail.com>
2026-04-01 18:13:43 +0400

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""Module for converting a class instance to a dictionary."""
+
+
 def class_to_json(obj):
+    """Returns the dictionary description of an object for JSON serialization.
+
+    Args:
+        obj: The instance of a class.
+
+    Returns:
+        dict: The dictionary representation of the object.
+    """
     return obj.__dict__
