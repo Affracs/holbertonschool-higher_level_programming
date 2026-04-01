d16898dd945c80057abd231d45965b883c4ca420
Affracs <alexfrancois1501@gmail.com>
2026-04-01 14:27:34 +0400

    6

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module for loading a Python object from a JSON file."""
+"""Module for saving a Python object to a JSON file."""
 
 import json
 
 
-def load_from_json_file(filename):
-    """Creates an object from a JSON file.
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using a JSON representation.
 
     Args:
-        filename (str): The name of the file to read from.
+        my_obj: The Python object to serialize.
+        filename (str): The name of the file to write to.
 
     Returns:
-        object: The Python object represented by the JSON file.
+        None
     """
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Module for loading a Python object from a JSON file."""
+
 import json
 
+
 def load_from_json_file(filename):
+    """Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file to read from.
+
+    Returns:
+        object: The Python object represented by the JSON file.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
