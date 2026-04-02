4ef4b0b9e1f33041dea0d8f4b0c0fb9b178fc19e
Affracs <alexfrancois1501@gmail.com>
2026-04-02 10:24:16 +0400

    10

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
+"""Module for defining a Student class with serialization and deserialization."""
+
+
 class Student:
+    """Defines a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Returns the dictionary representation of the instance.
+
+        Args:
+            attrs (list): List of attribute names to retrieve.
+
+        Returns:
+            dict: The dictionary representation of the student.
+        """
         if isinstance(attrs, list):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+
         return self.__dict__
 
     def reload_from_json(self, json):
+        """Replaces all attributes of the instance.
+
+        Args:
+            json (dict): A dictionary of attributes and values.
+        """
         for key, value in json.items():
             setattr(self, key, value)
